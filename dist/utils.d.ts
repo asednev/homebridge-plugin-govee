af68d11fc8c86d7bdaf3c192db90d532494f6729
Alexey <3236025+asednev@users.noreply.github.com>
2021-03-20 13:18:50 -0700

    1.1.1

---
+++ dist/utils.d.ts
@@ -0,0 +1,2 @@
+export declare function sanitizeDeviceName(s: string): string;
+//# sourceMappingURL=utils.d.ts.map
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+npm --no-git-tag-version version patch
 npm publish --tag beta
