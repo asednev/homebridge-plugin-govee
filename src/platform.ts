9e3c4d21c76221180628828ab286be918960713e
MrFly72 <thorsten@lambrecht.de>
2021-12-29 19:19:15 +0100

    Added code for ignore to src Dir

--- src/platform.ts
+++ src/platform.ts
@@ -108,6 +108,14 @@ export class GoveeHomebridgePlatform implements DynamicPlatformPlugin {
       );
       return;
     }
+    //Now check if the device is in the ignore list, if it is, skip working on it
+    const displayName = `${this.sanitize(reading.model)}`;
+    const IgnoredDeviceNames = this.config.IgnoreDeviceNames;
+    if( IgnoredDeviceNames.includes(displayName)) { // it will return true if you DeviceName is one of array values else false
+        //Device is in the Ignore-List so skip working on it
+        this.log.debug("Device in Ignore List", displayName," skipping the device!");
+        return;
+    }
 
     // discovered devices and register each one if it has not already been registered
 
